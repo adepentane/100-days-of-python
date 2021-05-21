ec6041f02400d9bb53d2e0836403897a9649dc83
adepentane <adepentane5@gmail.com>
2021-05-21 23:35:35 +0100

    Day 1 Start Band Name Project

---
+++ Day 1/start.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator \n")
+cityName = (input("Which city Did you grow up ins ? \n"))
+petName = (input("What is the name of your Pet ? \n"))
+print("Your Band Name could be " + cityName +" " + petName + "\n")
