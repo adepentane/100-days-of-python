8c9d143e1c636993b9c02a0dd3da9e6bd2c07639
adepentane <adepentane5@gmail.com>
2021-05-28 14:00:48 +0100

    The BMI Calculator Project

--- Day 2/BMICalculator.py
+++ Day 2/BMICalculator.py
@@ -3,6 +3,7 @@ height = input("Enter your Height in m: ")
 weight = input("Enter your Weight in kg: ")
 # Don't change the code above
 
-BMI = int(weight)/float(height)*float(height)
+BMI = int(weight)/float(height)** 2
+BMI_as_int = int(BMI)
 
-print("Your BMI is : " + str(BMI))
+print("Your BMI is : " + str(BMI_as_int))
